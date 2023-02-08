fa8422f5c70d07340618953ef25863abc953da7a
vagrant <vagrant@gp2f4a6.1>
2023-02-08 19:39:50 +0000

    Versió 1.3 Afegint canvis al codi HTML & PHP

--- app/codi.php
+++ app/codi.php
@@ -1,25 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+        <meta charset="utf-8">
+        <title>Preu de reserva</title>
+</head> 
+<body>
+        <h1><u>Preu de reserva</u></h1>
 
-$room_type = $_POST['room_type'];
-$room_price = 0;
-if ($room_type == "basic") {
-  $room_price = 40;
-} else {
-  $room_price = 50;
-}
+        <?php
 
-$num_rooms = $_POST['num_rooms'];
-$num_persons = $_POST['num_persons'];
-$num_doubles = $_POST['num_doubles'];
-$num_singles = $_POST['num_singles'];
-$num_nights = $_POST['num_nights'];
+        $room_type = $_POST['room_type'];
+        $room_price = 0;
+        if ($room_type == "basic") {
+          $room_price = 40;
+        } else {
+          $room_price = 50;
+        }
+        
+        $num_rooms = $_POST['num_rooms'];
+        $num_persons = $_POST['num_persons'];
+        $num_doubles = $_POST['num_doubles'];
+        $num_singles = $_POST['num_singles'];
+        $num_nights = $_POST['num_nights'];
 
-$total_price = ($room_price * $num_nights * $num_rooms * $num_persons) +
-               ($num_doubles * 15) +
-               ($num_singles * 10);
+        $total_price = ($room_price * $num_nights * $num_rooms * $num_persons) +
+                       ($num_doubles * 15) +
+                       ($num_singles * 10);
+        
+        $preu_total = "El preu total de la reserva és de: " . $total_price . "€\n";
+        
+        echo $preu_total;
 
-$preu_total = "El preu total de la reserva és de: " . $total_price . "€\n";
+        ?>
+        <br>
+        <a href="index.html">Tornar a la pagina anterior</a>
+</body>
+</html> 
 
-echo $preu_total;
-
-?>
